9c26a7eb84b6839c00921ce146e337f95e89f6a6
Craig <ctinney94@gmail.com>
2016-07-12 17:11:12 +0100

    Press down to drop through platforms (not setup correctly for 2 player yet)

--- Assets/Scripts/platform.cs
+++ Assets/Scripts/platform.cs
@@ -5,29 +5,39 @@ public class platform : MonoBehaviour {
 
     public Collider2D thing;
     bool Player1, Player2;
+    Collider2D playerGuy;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (Player1)
-            Physics2D.IgnoreCollision(col, thing, false);
+        if (col.tag == "Player")
+        {
+            Player1 = true;
+            playerGuy = col;
+        }
+        Physics2D.IgnoreCollision(col, thing, false);
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
-        Player1 = true;
+        if (col.tag == "Player")
+            Player1 = false;
+        
         Physics2D.IgnoreCollision(col, thing,true);
     }
 
 
-    void Start ()
+    void Start()
     {
-	
-	}
-	
-	void Update ()
-    {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            thing.enabled = false;
-        }
+
     }
+
+    void Update()
+    {
+        if (Player1)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Physics2D.IgnoreCollision(playerGuy, thing, true);
+            }
+        }
+    }    
 }
