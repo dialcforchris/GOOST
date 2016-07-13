552e5ab112fc7cf7017c646fdeb1ebfb0ff005fb
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-07-13 14:19:34 +0100

    Platform Fix

--- Assets/Scripts/platform.cs
+++ Assets/Scripts/platform.cs
@@ -1,46 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class platform : MonoBehaviour {
-
+public class platform : MonoBehaviour
+{
     public Collider2D thing;
     void OnTriggerEnter2D(Collider2D col)
     {
-<<<<<<< HEAD
-        if (col.tag == "Player")
-        {
-            Player1 = true;
-            playerGuy = col;
-            Physics2D.IgnoreCollision(col, thing, false);
-        }
-        
-    }
-
-    void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.tag == "Player")
-        {
-            Player1 = false;
-            Physics2D.IgnoreCollision(col, thing, true);
-        }
-
-=======
         Physics2D.IgnoreCollision(col, thing, false);
+
     }
 
     void OnTriggerExit2D(Collider2D col)
-    {        
-        Physics2D.IgnoreCollision(col, thing,true);
->>>>>>> origin/master
-    }
-
-
-    void Start()
     {
+        Physics2D.IgnoreCollision(col, thing, true);
 
     }
-
-    void Update()
-    {
-    }    
 }
