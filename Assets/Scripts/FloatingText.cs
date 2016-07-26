e16270508f74551144c9fb16c1b8e4c8ebb32723
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-07-26 09:07:09 +0100

    huge merge fuckup

---
+++ Assets/Scripts/FloatingText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour, IPoolable<FloatingText>
+{
+    #region IPoolable
+    public PoolData<FloatingText> poolData { get; set; }
+    #endregion
+
+    
+    [SerializeField]
+    Text score;
+    private bool onOff = true;
+	
+    void Update()
+    {
+        if (onOff)
+        {
+            score.color = new Color(score.color.r, score.color.g, score.color.b, score.color.a - Time.deltaTime/2);
+            score.rectTransform.position = new Vector2 (score.rectTransform.position.x,score.rectTransform.position.y + Time.deltaTime);
+        }
+        if (score.color.a<=0)
+        {
+            ReturnPool();
+        }
+    }
+
+    public void OnPooled(int _score,Vector2 _position, Color _colour)
+    {
+        gameObject.SetActive(true);
+        score.color = new Color( _colour.r,_colour.g,_colour.b,1);
+        score.text = _score.ToString();
+        score.rectTransform.position = _position;
+    }
+
+    public void ReturnPool()
+    {
+        poolData.ReturnPool(this);
+        gameObject.SetActive(false);
+    }
+
+  
+}
---
+++ Assets/Scripts/FloatingTextPool.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingTextPool : MonoBehaviour 
+{
+    private static FloatingTextPool textPool = null;
+    public static FloatingTextPool instance { get { return textPool; } }
+
+    private ObjectPool<FloatingText> objectPool = null;
+    [SerializeField]
+    private FloatingText floatingTextPrefab = null;
+
+    void Awake()
+    {
+        textPool = this;
+
+    }
+    private void Start()
+    {
+        objectPool = new ObjectPool<FloatingText>(floatingTextPrefab, 10, transform);
+    }
+
+    public FloatingText PoolText(int _score, Vector2 _position, Color _colour)
+    {
+        Debug.Log("pooled Text");
+        FloatingText _floatingText = objectPool.GetPooledObject();
+        _floatingText.OnPooled(_score,_position,_colour);
+        return _floatingText;
+    }
+}
