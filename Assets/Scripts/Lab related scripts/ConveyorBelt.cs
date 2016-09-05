c74474a5b0398b4e5b68eeabe8cb60ee239bbeb7
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-09-05 15:47:11 +0100

    working nice

--- Assets/Scripts/Lab related scripts/ConveyorBelt.cs
+++ Assets/Scripts/Lab related scripts/ConveyorBelt.cs
@@ -8,20 +8,12 @@ public class ConveyorBelt : MonoBehaviour
 
     float maxSpeed=1.25f;
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        Debug.Log(System.DateTime.Now.Millisecond);
-    }
     void OnCollisionStay2D(Collision2D col)
     {
         if (Mathf.Abs(col.gameObject.GetComponent<Rigidbody2D>().velocity.x) < maxSpeed)
         {
             col.gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2((direction ? 5 : -5) * Time.deltaTime * 10, 0);
-            //if (col.gameObject.GetComponent<Rigidbody2D>().velocity.x > maxSpeed)
-            //    col.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(maxSpeed, col.gameObject.GetComponent<Rigidbody2D>().velocity.y);
-
-            //if (col.gameObject.GetComponent<Rigidbody2D>().velocity.x < -maxSpeed)
-            //    col.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-maxSpeed, col.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+      
         }
         col.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(col.gameObject.GetComponent<Rigidbody2D>().velocity.x, 0);
     }
